c5280fdf373fea84cd8431aefa20596ea70fadf7
ravichandar8 <ravichandar.v@gmail.com>
2016-10-12 20:12:18 +0530

    SRC

---
+++ GitTutorialProject/src/com/git/learning/Main.java
@@ -0,0 +1,17 @@
+package com.git.learning;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("Test Message 1");
+		
+		System.out.println("test Message 2");
+	}
+
+}
---
+++ GitTutorialProject/src/com/git/learning/SalesData.java
@@ -0,0 +1,8 @@
+package com.git.learning;
+
+public class SalesData {
+	
+	public void display(){
+		System.out.println("Displaying This Data");
+	}
+}
